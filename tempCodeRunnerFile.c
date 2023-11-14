e7c7f4c7e560f73ce8142f87e6de0f29046380d6
Sanyam302 <sj5295673@gmail.com>
2023-11-14 10:34:27 +0530

    a

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 
 int main(){
-    int i,j;
-    for(j=1;j<=5;j++){
-        for(i=1;i<=5;i++){
-            printf("*");
-            
+    int i,j, n , m;
+    printf("enter number");
+    scanf("%d %d",&n,&m);
+    for(j=1;j<=n;j++){
+        for(i=1;i<=m;i++){
+        printf("*");
         }printf("\n");
 
     }
     return 0;
+
 }
